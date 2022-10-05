c65c82f818c86364bcbb38582c440f6fe9c92a89
Victor <victorallesund@gmail.com>
2022-10-05 14:41:34 +0300

    improvement

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -6,16 +6,39 @@ public class MainClass
     {
         var A = new Matrix();
         A.Read();
-        double n = double.Parse(Console.ReadLine());
-        A.Multiply(n);
-        A.Write();
+        var T = Matrix.MatrixTransposition(A);
+        T.Write();
+
+        //double n = double.Parse(Console.ReadLine());
+        //var M = Matrix.MultiplyByNumber(n, A);
+        //A.Write();
+
+        var B = new Matrix();
+        B.Read();
+
+        //var K = Matrix.Sum(A, B);
+        //A.Write();
+        //var C = Matrix.MatrixMultiply(A, B);
+        //C.Write();
+
+        var R = Matrix.MatrixMultiply(Matrix.Sum(Matrix.MultiplyByNumber(5, A), Matrix.MultiplyByNumber(2, B)), A);
+        R.Write();
     }
 }
 public class Matrix
 {
-    public double[,] data;
-    public byte rows;
-    public byte columns;
+    private double[,] data;
+    private byte rows;
+    private byte columns;
+    
+    //Конструкторы
+    public Matrix() { }
+    public Matrix(byte rows, byte columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new double[rows, columns];
+    }
     ///<summary>
     ///Метод считывает с консоли двухмерный массив
     ///</summary>
@@ -53,14 +76,72 @@ public class Matrix
         }
     }
     ///<summary>Метод выводит на консоль результат умножения матрицы на число</summary>
-    public void Multiply(double n)
+    static public Matrix MultiplyByNumber(double n, Matrix A)
     {
-        for (byte i = 0; i < rows; i++)
+        Matrix C = new Matrix(A.rows, A.columns);
+        for (byte i = 0; i < A.rows; i++)
         {
-            for (byte j = 0; j < columns; j++)
+            for (byte j = 0; j < A.columns; j++)
             {
-                data[i,j] *= n;
+                C.data[i, j] = A.data[i, j] * n;
             }
         }
+        return C;
+    }
+    static public Matrix Sum(Matrix A,Matrix B)
+    {
+        if (A.rows == B.rows && A.columns == B.columns)
+        {
+            Matrix C = new Matrix(A.rows, A.columns);
+            for (byte i = 0; i < A.rows; i++)
+            {
+                for (byte j = 0; j < A.columns; j++)
+                {
+                    C.data[i, j] = A.data[i, j] + B.data[i, j];
+                }
+            }
+            return C;
+        }
+        else
+        {
+            Console.WriteLine("Матрицы разного размера!");
+            return null;
+        }
+    }
+    static public Matrix MatrixMultiply(Matrix A, Matrix B)
+    {
+        if (A.columns == B.rows)
+        {
+            Matrix C = new Matrix(A.rows, B.columns);
+            //C.data = new double[C.rows, C.columns];
+            for (byte i = 0; i < A.rows; i++)
+            {
+                for(byte j = 0; j < B.columns; j++)
+                {
+                    for (byte k = 0; k < A.columns; k++)
+                    {
+                        C.data[i,j] += A.data[i,k] * B.data[k,j];
+                    }
+                }
+            }
+            return C;
+        }
+        else
+        {
+            Console.WriteLine("Для умножения матриц кол-во столбцов первой матрицы должно быть равно кол-ву строк второй матрицы!");
+            return null;
+        }
+    }
+    static public Matrix MatrixTransposition(Matrix A)
+    {
+        Matrix B = new Matrix(A.columns, A.rows);
+        for (byte i = 0; i < A.rows; i++)
+        {
+            for (byte j = 0; j < A.columns; j++)
+            {
+                B.data[j,i] = A.data[i,j];
+            }
+        }
+        return B;
     }
 }
